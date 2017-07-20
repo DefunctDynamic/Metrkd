4b7257ad378019a008f5d3d6e88f8903228cc20e
Ranjeet Singh <ranjeet@raeoks.com>
2017-07-20 08:36:53 +0530

    Move udp_server.js into server.js

--- test/udp_server.test.js
+++ test/udp_server.test.js
@@ -1,30 +1,29 @@
 const dgram      = require("dgram");
 const { expect } = require("chai");
 
-const UDPServer = require("../lib/udp_server");
+const { UDPServer } = require("../lib/server");
 
 describe("UDPServer", () => {
   describe("#accept", () => {
     it("SHOULD receive UDP data packet", (done) => {
       const type    = "udp4";
-      const config  = {
-        address: "127.0.0.1",
-        port   : 9125
-      };
+      const address = "127.0.0.1";
+      const port    = 9125;
+
       const message = "Hey! Ya";
 
-      const server = new UDPServer(type, config);
-      expect(server.isStarted()).to.be.false;
+      const server = new UDPServer(type, address, port);
+      expect(server.isClosed()).to.be.true;
       server.accept((data, rinfo) => {
         expect(message).to.be.equal(data.toString());
         expect(message.length).to.be.equal(rinfo.size);
         done();
       });
-      expect(server.isStarted()).to.be.true;
+      expect(server.isListening()).to.be.true;
 
       const buffer = new Buffer(message);
       const socket = dgram.createSocket("udp4");
-      socket.send(buffer, 0, buffer.length, config.port, config.address, () => {
+      socket.send(buffer, 0, buffer.length, port, address, () => {
         socket.close();
       });
     });
