9f983b9a1dcae6ea95a2fb2869dd0bca6a4c3c5e
Ranjeet Singh <ranjeet@raeoks.com>
2017-07-20 09:26:36 +0530

    Add TCP server with to check data transfer

---
+++ test/server.test.js
@@ -0,0 +1,61 @@
+const dgram = require("dgram");
+const net   = require("net");
+
+const { expect } = require("chai");
+
+const {
+        UDPServer,
+        TCPServer,
+      } = require("../lib/server");
+
+describe("UDPServer", () => {
+  describe("#accept", () => {
+    it("SHOULD accept UDP data packet", (done) => {
+      const type    = "udp4";
+      const address = "127.0.0.1";
+      const port    = 9125;
+
+      const message = "foobar:1337\n";
+
+      const server = new UDPServer(type, address, port);
+      expect(server.isClosed()).to.be.true;
+      server.accept((data, rinfo) => {
+        expect(message).to.be.equal(data.toString());
+        expect(message.length).to.be.equal(rinfo.size);
+        done();
+      });
+      expect(server.isListening()).to.be.true;
+
+      const buffer = new Buffer(message);
+      const socket = dgram.createSocket(type);
+      socket.send(buffer, 0, buffer.length, port, address, () => {
+        socket.close();
+      });
+    });
+  });
+});
+
+describe("TCPServer", () => {
+  describe("#accept", () => {
+    it("SHOULD accept TCP data packet", (done) => {
+      const address = "127.0.0.1";
+      const port    = 10125;
+
+      const message = "foobar:1337\n";
+
+      const server = new TCPServer(address, port);
+      expect(server.isClosed()).to.be.true;
+      server.accept((data, rinfo) => {
+        expect(message).to.be.equal(data.toString());
+        expect(message.length).to.be.equal(rinfo.size);
+        done();
+      });
+      expect(server.isListening()).to.be.true;
+
+      const client = net.connect(port, address, () => {
+        client.write(message);
+        client.end();
+      });
+    });
+  });
+});
