bba2fa0d4c4d38a2c4c6442670d8859c4f808151
Ranjeet Singh <ranjeet@raeoks.com>
2017-07-23 18:19:01 +0530

    Add configurator and stated cli

---
+++ main.js
@@ -0,0 +1,15 @@
+const config = require("./lib/config");
+
+function main(configPath) {
+  try {
+    config.load(configPath);
+    global.console.log(config.get("servers"));
+  } catch (error) {
+    global.console.error({
+      message: "failed to load config",
+      error  : error
+    });
+  }
+}
+
+module.exports = main;
